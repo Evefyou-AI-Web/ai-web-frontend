f815afae8a400bd4d7fa649c03463df1c73dcc7a
evef <eveforest@163.com>
2023-06-10 19:23:28 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
-// import { ViteAliases } from 'vite-aliases'
 import { generateModifyVars } from './build/generate/generateModifyVars'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
